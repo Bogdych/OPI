adac0c27b4c0335b3a66035cb449c851bfe5033d
Bogdan Babkevich <babkevich_bm_25@student.grsu.by>
2025-10-28 10:55:39 +0300

    A2.py

--- Lab4/A2.py
+++ Lab4/A2.py
@@ -1,30 +1,23 @@
-a = input("Введите какую фигуру вы хотите нарисовать (P T R): ")
-b= ['P','T','R']
-for l in b:
-    if a != b:
-        break
-    else:
-        print('Введите корректную букву!')
 n = int(input("Введите кол-во строк: "))
 m = int(input("Введите кол-во столбцов: "))
 
-if a == "P":
-    for i in range(n):
-        for j in range(m):
-            print("&", end="")
-        print()
 
-elif a == "T":
-    for i in range(1, n + 1):
-        for j in range(i):
-            print("&", end="")
-        print()
+for i in range(n):
+    for j in range(m):
+        print("&", end="")
+    print()
 
-elif a == "R":
-    for i in range(n):
-        for j in range(m):
-            if i == 0 or i == n - 1 or j == 0 or j == m - 1:
-                print("&", end="")
-            else:
-                print(" ", end="")
-        print()
+print("\n")
+for i in range(1, n + 1):
+    for j in range(i):
+        print("&", end="")
+    print()
+
+print("\n")
+for i in range(n):
+    for j in range(m):
+        if i == 0 or i == n - 1 or j == 0 or j == m - 1:
+            print("&", end="")
+        else:
+            print(" ", end="")
+    print()
